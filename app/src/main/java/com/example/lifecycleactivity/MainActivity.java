93d32a8be55dad721142372378347b15039cd694
Nam Phung <namhien12082003@gmail.com>
2023-09-11 21:56:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lifecycleactivity/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.lifecycleactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mBtnNavigateScreen2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("BBB", "onCreate");
+
+        mBtnNavigateScreen2 = findViewById(R.id.btnNavigateScreen2);
+        mBtnNavigateScreen2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("BBB", "onStart");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("BBB", "onRestart");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("BBB", "onResume");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("BBB", "onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("BBB", "onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("BBB", "onDestroy");
+    }
+}
